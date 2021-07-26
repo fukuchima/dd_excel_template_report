fa439069075ce43ec3c5dcdf609a083639d06e5c
Masayuki FUKUCHI <masayuki.fukuchi@grapecity.com>
2021-07-26 17:51:56 +0900

    initial commit

---
+++ Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace DDExcelReport
+{
+
+    // 顧客情報
+    public class Customer
+    {
+        public string companyname { get; set; }
+        public string name { get; set; }
+        public string postalcode { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string tel { get; set; }
+        public Detail[] detail { get; set; }
+    }
+
+    // 明細
+    public class Detail
+    {
+        public string sku { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public int unit { get; set; }
+        public string remark { get; set; }
+    }
+
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,18 @@
+﻿namespace DDExcelReport
+{
+
+    // 発行元情報
+    public class Publisher
+    {
+        public string companyname { get; set; }
+        public string postalcode { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string tel { get; set; }
+        public string bankname { get; set; }
+        public string bankblanch { get; set; }
+        public string account { get; set; }
+        public string representative { get; set; }
+    }
+
+}
